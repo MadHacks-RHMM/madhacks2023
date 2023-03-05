0d1944be907602e75655678ec93535e3bf633468
Rusheel Dasari <rusheel@gmail.com>
2023-03-05 03:53:36 -0600

    Finished initial draft of backend code

--- backend-python/database.py
+++ backend-python/database.py
@@ -1,4 +1,5 @@
 import pymongo
+import datetime as dt
 from datetime import datetime
 import sys
 
@@ -10,9 +11,26 @@ class UserData:
         self.banks: list[str] = []
         self.bank_ids: list[str] = []
         self.bank_account_tokens: dict[str, dict[str, str]] = []
-        self.last_rounded: datetime = None
+        self.last_rounded: str = str(datetime(dt.MINYEAR, 1, 1))
         self.name: str = ""
         self.email: str = ""
+
+    def __iter__(self):
+        for key in self.__dict__:
+            yield key, getattr(self, key)
+
+    def __init__(self, data: dict):
+        if data is None:
+            self = None
+            return
+        self.access_tokens = data['access_tokens']
+        self.account_ids = data['account_ids']
+        self.banks = data['banks']
+        self.bank_ids = data['bank_ids']
+        self.bank_account_tokens = data['bank_account_tokens']
+        self.last_rounded = data['last_rounded']
+        self.name = data['name']
+        self.email = data['email']
 
 
 class MyDB:
@@ -29,10 +47,13 @@ class MyDB:
             # use a database named "AppDB"
 
         self.db = self.client.AppDB
-        print(self.db.name)
 
-    def get_user(self, email: str) -> UserData:
-        pass
+    def get_user(self, email: str) -> UserData | None:
+        return UserData(self.db.find({"email": email}))
+
+    def set_user(self, user: UserData) -> None:
+        return None if self.db.update_one({"email": user.email}, {
+            "$set": dict(user)}, upsert=True) else None
 
 
-# MyDB()
+print(MyDB().db.list_collection_names())
--- backend-python/main.py
+++ backend-python/main.py
@@ -15,17 +15,43 @@ class FastApp:
             allow_methods=['*'],
             allow_headers=['*']
         )
+        self.user = None
 
 
 def main():
     fastapp = FastApp()
+    database = MyDB()
+    plaid = PlaidData('Development')
 
     @fastapp.app.get('/')
     async def root():
         return {'message': 'Fetch Connection Successful'}
 
+    @fastapp.app.post('/login')
+    async def login(email: str = ""):
+        user = database.get_user(email)
+        if user is None:
+            return {'message': 'User Not Found'}
+
+        fastapp.user = user
+        return {'message': 'Login Successful'}
+
+    @fastapp.app.get('/get_transactions')
+    async def get_transactions():
+        if (fastapp.user is None):
+            return {'message': 'User Not Found'}
+        else:
+            return {"message": "Transactions Found", "transactions": plaid.get_transactions(fastapp.user)}
+
     @fastapp.app.post('/link_success_data')
     async def get_client_id(link_data: LinkData):
+        user = database.get_user(link_data.email)
+        user = plaid.make_account(
+            link_data) if user is None else plaid.update_account(link_data, user)
+        database.set_user(user)
+
+        fastapp.user = user
+
         return {'message': 'Link Success Data Received', 'client_id': link_data.accounts['id']}
 
     uvicorn.run(fastapp.app, host='localhost')
