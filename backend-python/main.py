21c4a714b03a44eeadff2ebff5d27bb8c24cb6a8
Rusheel Dasari <rusheel@gmail.com>
2023-03-04 20:53:37 -0600

    added some plaid code

--- backend-python/main.py
+++ backend-python/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from flask import jsonify
 from plaid.api import plaid_api
 from plaid.model.transactions_sync_request import TransactionsSyncRequest
 from plaid.model.item_public_token_exchange_request import ItemPublicTokenExchangeRequest
+from plaid.model.link_token_create_request import LinkTokenCreateRequest
+from plaid.model.link_token_create_request_user import LinkTokenCreateRequestUser
+from plaid.model.products import Products
+from plaid.model.country_code import CountryCode
 import uvicorn
 import plaid
 
@@ -10,6 +15,17 @@ import plaid
 PLAID_CLIENT_ID = "6403cf9acca1db0012448611"
 PLAID_SECRET_ID_SANDBOX = "03f8cb25a85a0d46070aea16b08122"
 PLAID_SECRET_ID_DEVELOPMENT = "ce796b1f5343a3fce5c0c9b792260d"
+
+
+class ClientInfo:
+    client_id = ""
+    access_token = ""
+    item_id = ""
+
+    def __init__(self, client_id="", access_token="", item_id=""):
+        self.client_id = client_id
+        self.access_token = access_token
+        self.item_id = item_id
 
 
 def main():
@@ -22,9 +38,16 @@ def main():
         allow_headers=['*']
     )
 
+    client_info = ClientInfo()
+
     @fastapp.get('/')
-    def root():
+    async def root():
         return {'message': 'Fetch Connection Successful'}
+
+    @fastapp.post('/get_client_id')
+    async def get_client_id(client_id: str):
+        client_info.client_id = client_id
+        return {'message': 'Client ID Received'}
 
     host = plaid.Environment.Sandbox
 
@@ -39,6 +62,23 @@ def main():
 
     api_client = plaid.ApiClient(configuration)
     client = plaid_api.PlaidApi(api_client)
+
+    @fastapp.post('/create_link_token')
+    async def create_link_token():
+        request = LinkTokenCreateRequest(
+            products=[Products("auth")],
+            client_name="Plaid Test App",
+            country_codes=[CountryCode('US')],
+            redirect_uri='https://domainname.com/oauth-page.html',
+            language='en',
+            webhook='https://webhook.example.com',
+            user=LinkTokenCreateRequestUser(
+                    client_user_id=client_info.client_id
+            )
+        )
+        response = client.link_token_create(request)
+
+        return jsonify(response.to_dict())
 
     uvicorn.run(fastapp, host='localhost')
 
