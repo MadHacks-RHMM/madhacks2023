40ee3c6e379a73daf915dc62cede51a2d010ce73
Rusheel Dasari <rusheel@gmail.com>
2023-03-04 18:27:55 -0600

    added Plaid client

--- backend-python/main.py
+++ backend-python/main.py
@@ -7,6 +7,11 @@ import uvicorn
 import plaid
 
 
+PLAID_CLIENT_ID = "6403cf9acca1db0012448611"
+PLAID_SECRET_ID_SANDBOX = "03f8cb25a85a0d46070aea16b08122"
+PLAID_SECRET_ID_DEVELOPMENT = "ce796b1f5343a3fce5c0c9b792260d"
+
+
 def main():
     fastapp = FastAPI()
     fastapp.add_middleware(
@@ -20,6 +25,20 @@ def main():
     @fastapp.get('/')
     def root():
         return {'message': 'Fetch Connection Successful'}
+
+    host = plaid.Environment.Sandbox
+
+    configuration = plaid.Configuration(
+        host=host,
+        api_key={
+            'clientId': PLAID_CLIENT_ID,
+            'secret': PLAID_SECRET_ID_SANDBOX,
+            'plaidVersion': '2020-09-14'
+        }
+    )
+
+    api_client = plaid.ApiClient(configuration)
+    client = plaid_api.PlaidApi(api_client)
 
     uvicorn.run(fastapp, host='localhost')
 
